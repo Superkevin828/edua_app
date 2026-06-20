b1f5308cc11041ec9702b6c8961109c87c2ad650
superkevin <lyavalakevin@gmail.com>
2026-06-20 21:56:38 +0300

    pack

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ try{
 // Middleware
 app.use(helmet({ contentSecurityPolicy: false, crossOriginEmbedderPolicy: false }));
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://0.0.0.0:3000', 'http://localhost:5000'],
+    origin: ['http://localhost:3000','https://edua-app.pages.dev'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
