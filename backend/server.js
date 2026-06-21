578bd6e6998ea2ca7d067b28c9f3e07bd9bf916f
superkevin <lyavalakevin@gmail.com>
2026-06-21 10:45:22 +0300

    time exp

--- backend/server.js
+++ backend/server.js
@@ -39,8 +39,8 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(morgan('dev'));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 // ============================================
 // API Routes
